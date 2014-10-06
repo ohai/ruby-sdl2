3ee6c1c1aec81089bf245cc99c3f5c104610df24
Ippei Obayashi <ohai@kmc.gr.jp>
2014-10-06 18:33:24 +0900

    Clean up testsprite and use SDL::Key::Scan::* for the test script

--- sample/testsprite.rb
+++ sample/testsprite.rb
@@ -6,19 +6,11 @@ window = SDL2::Window.create("testsprite",
                              SDL2::Window::OP_CENTERED, SDL2::Window::OP_CENTERED,
                              640, 480, 0)
 renderer = window.create_renderer(-1, 0)
-p window.debug_info
-p renderer.debug_info
 icon = SDL2::Surface.load_bmp("icon.bmp")
-p icon.destroy?
-
 texture = renderer.create_texture_from(icon)
 icon.destroy
 
-p icon.destroy?
 rect = SDL2::Rect.new(48, 128, 32, 32)
-p rect.x
-rect.x = 32
-p rect.x
 
 renderer.copy(texture, nil, rect)
 renderer.present
@@ -27,7 +19,7 @@ loop do
   while ev = SDL2::Event.poll
     case ev
     when SDL2::Event::KeyDown
-      if ev.scancode == 41
+      if ev.scancode == SDL2::Key::Scan::ESCAPE
         exit
       else
         p ev.scancode
