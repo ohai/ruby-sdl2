85289609bbbf69175b46670b04db57a412cb1c4e
Ippei Obayashi <ohai@kmc.gr.jp>
2014-10-10 00:11:04 +0900

    Add Mixer.fadein_channel

--- mixer.c
+++ mixer.c
@@ -6,6 +6,9 @@ static VALUE mMixer;
 static VALUE cChunk;
 static VALUE cMusic;
 
+static VALUE playing_chunks = Qnil;
+static VALUE playing_music = Qnil;
+
 #define MIX_ERROR() do { HANDLE_ERROR(SDL_SetError(Mix_GetError())); } while(0)
 #define HANDLE_MIX_ERROR(code) \
     do { if ((code) < 0) { MIX_ERROR(); } } while (0)
@@ -67,7 +70,7 @@ static VALUE Mixer_s_open(int argc, VALUE* argv, VALUE self)
                                    (format == Qnil) ? MIX_DEFAULT_FORMAT : NUM2UINT(format),
                                    (channels == Qnil) ? 2 : NUM2INT(channels),
                                    (chunksize == Qnil) ? 1024 : NUM2INT(chunksize)));
-
+    playing_chunks = rb_ary_new();
     return Qnil;
 }
 
@@ -87,10 +90,36 @@ static VALUE Mixer_s_query(VALUE self)
                        INT2NUM(channels), INT2NUM(num_opened));
 }
 
-static VALUE Mixer_s_play_channel(VALUE self, VALUE channel, VALUE chunk, VALUE loops)
+static void protect_playing_chunk_from_gc(int channel, VALUE chunk)
 {
-    HANDLE_MIX_ERROR(Mix_PlayChannel(NUM2INT(channel), Get_Mix_Chunk(chunk),
-                                     NUM2INT(loops)));
+    rb_ary_store(playing_chunks, channel, chunk);
+}
+
+static VALUE Mixer_s_play_channel(int argc, VALUE* argv, VALUE self)
+{
+    VALUE channel, chunk, loops, ticks;
+    int ch;
+    rb_scan_args(argc, argv, "31", &channel, &chunk, &loops, &ticks);
+    if (ticks == Qnil)
+        ticks = INT2FIX(-1);
+    ch = Mix_PlayChannelTimed(NUM2INT(channel), Get_Mix_Chunk(chunk),
+                              NUM2INT(loops), NUM2INT(ticks));
+    HANDLE_MIX_ERROR(ch);
+    protect_playing_chunk_from_gc(ch, chunk);
+    return Qnil;
+}
+
+static VALUE Mixer_s_fadein_channel(int argc, VALUE* argv, VALUE self)
+{
+    VALUE channel, chunk, loops, ms, ticks;
+    int ch;
+    rb_scan_args(argc, argv, "41", &channel, &chunk, &loops, &ms, &ticks);
+    if (ticks == Qnil)
+        ticks = INT2FIX(-1);
+    ch = Mix_FadeInChannelTimed(NUM2INT(channel), Get_Mix_Chunk(chunk),
+                                NUM2INT(loops), NUM2INT(ms), NUM2INT(ticks));
+    HANDLE_MIX_ERROR(ch);
+    protect_playing_chunk_from_gc(ch, chunk);
     return Qnil;
 }
 
@@ -165,7 +194,8 @@ void rubysdl2_init_mixer(void)
     rb_define_module_function(mMixer, "open", Mixer_s_open, -1);
     rb_define_module_function(mMixer, "close", Mixer_s_close, 0);
     rb_define_module_function(mMixer, "query", Mixer_s_query, 0);
-    rb_define_module_function(mMixer, "play_channel", Mixer_s_play_channel, 3);
+    rb_define_module_function(mMixer, "play_channel", Mixer_s_play_channel, -1);
+    rb_define_module_function(mMixer, "fadein_channel", Mixer_s_fadein_channel, -1);
     rb_define_module_function(mMixer, "play_music", Mixer_s_play_music, 2);
     rb_define_module_function(mMixer, "play?", Mixer_s_play_p, 1);
     
@@ -191,7 +221,7 @@ void rubysdl2_init_mixer(void)
     rb_define_const(mMixer, "DEFAULT_FORMAT", UINT2NUM(MIX_DEFAULT_FORMAT));
     rb_define_const(mMixer, "DEFAULT_CHANNELS", INT2FIX(MIX_DEFAULT_CHANNELS));
     rb_define_const(mMixer, "MAX_VOLUME", INT2FIX(MIX_MAX_VOLUME));
-
+    
     
     cChunk = rb_define_class_under(mMixer, "Chunk", rb_cObject);
     rb_undef_alloc_func(cChunk);
@@ -208,6 +238,9 @@ void rubysdl2_init_mixer(void)
     rb_define_method(cMusic, "destroy", Music_destroy, 0);
     rb_define_singleton_method(cMusic, "load", Music_s_load, 1);
     rb_define_method(cMusic, "destroy?", Music_destroy_p, 0);
+
+    rb_gc_register_address(&playing_chunks);
+    rb_gc_register_address(&playing_music);
 }
 
 #else /* HAVE_SDL_MIXER_H */
--- sample/playwave.rb
+++ sample/playwave.rb
@@ -7,7 +7,7 @@ SDL2::Mixer.open(22050, SDL2::Mixer::DEFAULT_FORMAT, 2, 512)
 p SDL2::Mixer::Chunk.decoders
 wave = SDL2::Mixer::Chunk.load(ARGV[0])
 
-SDL2::Mixer.play_channel(0, wave, 0)
+SDL2::Mixer.fadein_channel(0, wave, 0, 600)
 
 while SDL2::Mixer.play?(0)
   sleep 1
