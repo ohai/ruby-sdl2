5b3e6590e6960ac242a642387ebd102dfa583b6c
Ippei Obayashi <ohai@kmc.gr.jp>
2014-12-10 17:02:51 +0900

    Update version

--- lib/sdl2/version.rb
+++ lib/sdl2/version.rb
@@ -1,6 +1,6 @@
 module SDL2
   # Version string of Ruby/SDL2
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
   # Version of Ruby/SDL2, [major, minor, patch level] 
-  VERSION_NUMBER = [0, 1, 0]
+  VERSION_NUMBER = [0, 2, 0]
 end
