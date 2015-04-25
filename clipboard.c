f694e198463c3c3caec0c809557915aa59a56165
Ippei Obayashi <ohai@kmc.gr.jp>
2015-04-25 19:12:03 +0900

    Update docstring of Clipboard module

--- clipboard.c
+++ clipboard.c
@@ -1,6 +1,19 @@
 #include "rubysdl2_internal.h"
 #include <SDL_clipboard.h>
 
+/*
+ * Document-module: SDL2::Clipboard
+ *
+ * This module has clipboard manipulating functions.
+ * 
+ */
+
+/*
+ * Get the text from the clipboard.
+ *
+ * @return [String] a string in the clipboard
+ * @return [nil] if the clipboard is empty
+ */ 
 static VALUE Clipboard_s_text(VALUE self)
 {
     if (SDL_HasClipboardText()) {
@@ -16,12 +29,22 @@ static VALUE Clipboard_s_text(VALUE self)
     }
 }
 
+/*
+ * Set the text in the clipboard.
+ *
+ * @param text [String] a new text
+ * @return [String] text
+ */
 static VALUE Clipboard_s_set_text(VALUE self, VALUE text)
 {
     HANDLE_ERROR(SDL_SetClipboardText(StringValueCStr(text)));
     return text;
 }
 
+/*
+ * Return true if the clipboard has any text.
+ *
+ */
 static VALUE Clipboard_s_has_text_p(VALUE self)
 {
     return INT2BOOL(SDL_HasClipboardText());
