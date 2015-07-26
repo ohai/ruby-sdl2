7ac60720971681defbbd077a4e0e59beeb33ea3c
Ippei Obayashi <ohai@kmc.gr.jp>
2015-07-26 19:11:26 +0900

    Update docstrings

--- filesystem.c
+++ filesystem.c
@@ -2,6 +2,16 @@
 #include "rubysdl2_internal.h"
 #include <SDL_filesystem.h>
 
+/*
+ * Get the directory where the application was run from.
+ *
+ * In almost all cases, using this method, you will get the directory where ruby executable is in.
+ * Probably what you hope is $0 or __FILE__.
+ * 
+ * @return [String] an absolute path in UTF-8 encoding to the application data directory
+ * @raise [SDL2::Error] raised if your platform doesn't implement this functionality.
+ * @note This method is available since SDL 2.0.1.
+ */
 static VALUE SDL2_s_base_path(VALUE self)
 {
     char* path = SDL_GetBasePath();
@@ -13,6 +23,23 @@ static VALUE SDL2_s_base_path(VALUE self)
     return str;
 }
 
+/*
+ * @overload preference_path(org, app)
+ *   Get the "pref dir". You can use the directory to write personal files such as
+ *   preferences and save games.
+ *   
+ *   The directory is unique per user and per application.
+ *
+ *   @param org [String] the name of your organization
+ *   @param app [String] the name of your application
+ *   @return [String] a UTF-8 string of the user directory in platform-depnedent notation.
+ * 
+ *   @raise [SDL2::Error] raised if your platform doesn't implement this functionality.
+ *   @note This method is available since SDL 2.0.1.
+ *
+ *   @example
+ *     SDL2.preference_path("foo", "bar") # => "/home/ohai/.local/share/foo/bar/" (on Linux)
+ */
 static VALUE SDL2_s_preference_path(VALUE self, VALUE org, VALUE app)
 {
     char* path = SDL_GetPrefPath(StringValueCStr(org), StringValueCStr(app));
