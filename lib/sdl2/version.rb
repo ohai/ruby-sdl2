b56691200ff6a148fd41a774cb942429632b49d7
Ippei Obayashi <ohai@kmc.gr.jp>
2020-02-02 16:33:20 +0900

    Bump version 0.3.5

--- lib/sdl2/version.rb
+++ lib/sdl2/version.rb
@@ -1,6 +1,6 @@
 module SDL2
   # Version string of Ruby/SDL2
-  VERSION = "0.3.4"
+  VERSION = "0.3.5"
   # Version of Ruby/SDL2, [major, minor, patch level] 
   VERSION_NUMBER = VERSION.split(".").map(&:to_i)
 end
