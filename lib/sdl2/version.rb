65dacb8defbfa4e357d2dad3ce32b4c08e61f029
Ippei Obayashi <ohai@kmc.gr.jp>
2026-04-22 11:32:44 +0900

    chore: bump version to 0.3.7

--- lib/sdl2/version.rb
+++ lib/sdl2/version.rb
@@ -1,6 +1,6 @@
 module SDL2
   # @return [String] Version string of Ruby/SDL2
-  VERSION = "0.3.6"
+  VERSION = "0.3.7"
   # @return [Array<Integer>] Version of Ruby/SDL2, [major, minor, patch level] 
   VERSION_NUMBER = VERSION.split(".").map(&:to_i)
 end
